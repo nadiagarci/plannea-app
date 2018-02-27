1016a4a2967cfe403bd0edb41305fd534a7def9e
Nadia Garcia <nadiagarci@hotmail.com>
2018-02-27 15:18:51 +0100

    Index_show_actions_views_created

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,12 @@
+# t.string "title"
+#     t.string "description"
+#     t.string "picture"
+#     t.string "location"
+#     t.integer "price"
+#     t.datetime "start_time"
+#     t.datetime "end_time"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+
 class Plan < ApplicationRecord
 end
