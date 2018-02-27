575e94f36983745351373cbb3702f2b74a9f4bcc
Almudena Vera <almudena.vigara@gmail.com>
2018-02-27 17:59:44 +0100

    added action in controller and added view new

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,14 @@
 class PlansController < ApplicationController
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.find(params[:id])
+    if @plan.save
+      redirect_to plan_path(@plan)
+    else
+      render :new
+  end
+end
 end
